dbc4438038b31c0f24447cd09c25b55c77b829fe
AliakseiM <aliaksei.markaran@gmail.com>
2022-11-16 11:19:14 +0100

    Chapter 7

---
+++ src/Chapter7/Rowboat.java
@@ -0,0 +1,7 @@
+package Chapter7;
+
+public class Rowboat extends Boat {
+    public void rowTheBoat() {
+        System.out.print("stroke natasha ");
+    }
+}
---
+++ src/Chapter7/Sailboat.java
@@ -0,0 +1,7 @@
+package Chapter7;
+
+public class Sailboat extends Boat {
+    public void move() {
+        System.out.println("hoist sail ");
+    }
+}
